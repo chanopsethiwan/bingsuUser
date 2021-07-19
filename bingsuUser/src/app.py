30d0524582ec2dfcf2b5358641092efab96f974c
chanopsethiwan <chanopsethiwan233@gmail.com>
2021-07-20 00:11:29 +0700

    added addUser function and global secondary index

--- bingsuUser/src/app.py
+++ bingsuUser/src/app.py
@@ -3,3 +3,20 @@ from .bingsuUser import PynamoBingsuUser
 
 def lambda_handler(event, context):
     return {'data': 'Hello World'}
+
+def add_user(event, context):
+#     user_item = PynamoBingsuUser(
+#         user_id = user_id,
+#         username = username,
+#         password = password,
+#         points = points,
+#         coins = coins,
+#         email = email,
+#         phone_number = phone_number,
+#         grab_id = grab_id,
+#         robinhood_id = robinhood_id,
+#         foodpanda_id = foodpanda_id,
+#         co2_amount = co2_amount
+#     )
+#     user_item.save()
+    return 'Hello World'
--- bingsuUser/src/bingsuUser.py
+++ bingsuUser/src/bingsuUser.py
@@ -4,14 +4,35 @@ from pynamodb.indexes import GlobalSecondaryIndex, AllProjection
 import os
 
 class PynamoBingsuUser(Model):
-    ''' database to store group '''
+    ''' database to store user '''
     class Meta:
         table_name = os.environ.get('BINGSU_USER_TABLE_NAME')
         region = 'ap-southeast-1'
-    userId = UnicodeAttribute(hash_key = True)
+    user_id = UnicodeAttribute(hash_key = True)
     username = UnicodeAttribute()
     password = UnicodeAttribute()
     points = NumberAttribute()
     coins = NumberAttribute()
     email = UnicodeAttribute()
+    phone_number = NumberAttribute()
+    grab_id = UnicodeAttribute(null=True)
+    robinhood_id = UnicodeAttribute(null=True)
+    foodpanda_id = UnicodeAttribute(null=True)
+    co2_amount = NumberAttribute(null=True)
+    
+class PointsIndex(GlobalSecondaryIndex):
+    """
+    This class represents a global secondary index
+    """
+    class Meta:
+        index_name = 'points'
+        read_capacity_units = 1
+        write_capacity_units = 1
+        # All attributes are projected
+        projection = AllProjection()
+
+    # This attribute is the hash key for the index
+    # Note that this attribute must also exist
+    # in the model
+    points = NumberAttribute(hash_key=True)
     

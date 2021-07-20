9e44e1f097366bfbcfc59692a0c8e32a5eace0e6
chanopsethiwan <chanopsethiwan233@gmail.com>
2021-07-21 02:53:04 +0700

    cut lines of code for add_user

--- bingsuUser/src/app.py
+++ bingsuUser/src/app.py
@@ -9,35 +9,20 @@ def lambda_handler(event, context):
 
 def add_user(event, context):
     item = event['arguments']
-    user_id = item['user_id']
-    username = item['username']
-    password = item['password']
-    grab_points = item.get('grab_points', None)
-    robinhood_points = item.get('robinhood_points', None)
-    foodpanda_points = item.get('foodpanda_points', None)
-    coins = item['coins']
-    email = item['email']
-    phone_number = item['phone_number']
-    grab_id = item.get('grab_id', None)
-    robinhood_id = item.get('robinhood_id', None)
-    foodpanda_id = item.get('foodpanda_id', None)
-    co2_amount = item['co2_amount']
-    
-    
     user_item = PynamoBingsuUser(
-        user_id = user_id,
-        username = username,
-        password = password,
-        grab_points = grab_points,
-        robinhood_points = robinhood_points,
-        foodpanda_points = foodpanda_points,
-        coins = coins,
-        email = email,
-        phone_number = phone_number,
-        grab_id = grab_id,
-        robinhood_id = robinhood_id,
-        foodpanda_id = foodpanda_id,
-        co2_amount = int(co2_amount)
+        user_id = item['user_id'],
+        username = item['username'],
+        password = item['password'],
+        grab_points = item.get('grab_points', None),
+        robinhood_points = item.get('robinhood_points', None),
+        foodpanda_points = item.get('foodpanda_points', None),
+        coins = item['coins'],
+        email = item['email'],
+        phone_number = item['phone_number'],
+        grab_id = item.get('grab_id', None),
+        robinhood_id = item.get('foodpanda_id', None),
+        foodpanda_id = item.get('foodpanda_id', None),
+        co2_amount = item['co2_amount']
     )
     user_item.save()
     return {'status': 200}
